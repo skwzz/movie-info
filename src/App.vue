b6ce99b4648fbfe77e9318a2932da5fb6e70c081
kiwoong <kidol9108@gmail.com>
2025-08-10 21:11:48 +0900

    vue3 반복문

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,54 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>영화정보</h1>
+
+    <!-- <div>
+      <h3 class="bg-yellow" :style="textRed">{{title}}</h3>
+      <p>개봉: {{year}}</p>
+      <p>장르: {{genre}}</p>
+    </div> -->
+    <div v-for="(item, i) in data" :key="i">
+      <h3 class="bg-yellow" :style="textRed">{{item.title}}</h3>
+      <p>개봉: {{item.year}}</p>
+      <p>장르: {{item.genre}}</p>
+      <button @click="increaseLike(i)">좋아요</button> <span>{{ item.like }}</span>
+    </div>
+
+    <p v-for="(item, i) in foods" :key="i">{{item}}</p>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  export default {
+    name: 'App',
+    data() {
+      return {
+        foods: ["김밥", "순대", "만두"],
+        data: [
+          {
+            title: "노량",
+            year: 2023,
+            genre: "액션, 드라마",
+            like: 0,
+          },
+          {
+            title: "아쿠아맨과 로스트킹덤",
+            year: 2023,
+            genre: "액션, 판타지, 어드벤처",
+            like: 0,
+          }  
+        ],
+        textRed: "color: red"
+      }
+    },
+    methods: {
+      increaseLike(i) {
+        this.data[i].like++;
+      }
+    }
   }
-}
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  .bg-yellow {
+    background: gold;
+  }
 </style>
