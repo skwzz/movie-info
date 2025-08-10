86a096bdb839c35a234901b3b141220ba05194b0
kiwoong <kidol9108@gmail.com>
2025-08-10 21:36:08 +0900

    데이터 외부파일로 이동시켜서 export, import 사용

--- src/App.vue
+++ src/App.vue
@@ -32,35 +32,14 @@
 </template>
 
 <script>
+  import data from './assets/movies';
+  console.log(data);
   export default {
     name: 'App',
     data() {
       return {
         isModal: false,
-        data: [
-          {
-            title: "노량",
-            year: 2023,
-            genre: "액션, 드라마",
-            imgUrl: './assets/노량.jpg',
-            like: 0,
-          },
-          {
-            title: "아쿠아맨과 로스트킹덤",
-            year: 2023,
-            genre: "액션, 판타지, 어드벤처",
-            imgUrl: './assets/아쿠아맨.jpg',
-            like: 0,
-          },
-          {
-            title: "3일의 휴가",
-            year: 2023,
-            genre: "드라마",
-            imgUrl: './assets/3일의휴가.jpg',
-            like: 0,
-          }, 
-        ],
-        textRed: "color: red"
+        data: data,
       }
     },
     methods: {
---
+++ src/assets/movies.js
@@ -0,0 +1,25 @@
+const data = [
+    {
+        title: "노량",
+        year: 2023,
+        genre: "액션, 드라마",
+        imgUrl: './assets/노량.jpg',
+        like: 0,
+    },
+    {
+        title: "아쿠아맨과 로스트킹덤",
+        year: 2023,
+        genre: "액션, 판타지, 어드벤처",
+        imgUrl: './assets/아쿠아맨.jpg',
+        like: 0,
+    },
+    {
+        title: "3일의 휴가",
+        year: 2023,
+        genre: "드라마",
+        imgUrl: './assets/3일의휴가.jpg',
+        like: 0,
+    }, 
+]
+
+export default data;
